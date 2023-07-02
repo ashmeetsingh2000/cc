bcf3baddbff2af40b721068cd54c4950484cbbf6
ashmeetsingh2000 <singhashmeet75@gmail.com>
2023-07-02 17:52:25 +0530

    faq page

--- js/course.js
+++ js/course.js
@@ -1,29 +1,29 @@
 
-let slideIndex = 1;
-show_testi_Slides(slideIndex);
+let course_slide_index = 1;
+show_testi_Slides(course_slide_index);
 
 function social_meida_plusSlides(n) {
-    show_testi_Slides(slideIndex += n);
+    show_testi_Slides(course_slide_index += n);
 }
 
 function social_currentSlide(n) {
-    show_testi_Slides(slideIndex = n);
+    show_testi_Slides(course_slide_index = n);
 }
 
 function show_testi_Slides(n) {
     let i;
     let slides = document.getElementsByClassName("social_Slides");
     let dots = document.getElementsByClassName("social_dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { course_slide_index = 1 }
+    if (n < 1) { course_slide_index = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[course_slide_index - 1].style.display = "block";
+    dots[course_slide_index - 1].className += " active";
 }
 
 
